bdc6636d2052651d353536da7cae52fbbcdcce1d
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2022-08-12 13:42:53 +0300

    Update positionappDB.sql

--- db/positionappDB.sql
+++ db/positionappDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 10, 2022 at 12:58 PM
+-- Generation Time: Aug 12, 2022 at 12:41 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,16 +29,43 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `groups` (
   `id` int(4) NOT NULL,
-  `groupcode` varchar(10) DEFAULT NULL,
-  `members` int(4) DEFAULT 1
+  `groupcode` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` (`id`, `groupcode`, `members`) VALUES
-(9, 'lDzWvR1wei', 3);
+INSERT INTO `groups` (`id`, `groupcode`) VALUES
+(9, 'lDzWvR1wei');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(4) NOT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `initials` varchar(2) DEFAULT NULL,
+  `color` varchar(7) DEFAULT NULL,
+  `groups_groupcode` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `message`, `initials`, `color`, `groups_groupcode`) VALUES
+(27, 'Hello, this is a test', 'MK', '#5BC0EB', 'lDzWvR1wei'),
+(28, 'Hello, this is also a test', 'TE', '#FF0000', 'lDzWvR1wei'),
+(29, 'test back', 'MK', '#5BC0EB', 'lDzWvR1wei'),
+(30, 'try again', 'TE', '#FF0000', 'lDzWvR1wei'),
+(31, 'testing', 'TE', '#FF0000', 'lDzWvR1wei'),
+(32, 'this is a longer message for testing the textboxes', 'TE', '#FF0000', 'lDzWvR1wei'),
+(33, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', 'MK', '#5BC0EB', 'lDzWvR1wei'),
+(34, 'hello', 'KO', '#AAACCB', 'lDzWvR1wei');
 
 -- --------------------------------------------------------
 
@@ -50,6 +77,8 @@ CREATE TABLE `positions` (
   `id` int(4) NOT NULL,
   `position` varchar(255) DEFAULT NULL,
   `uniqueID` varchar(10) DEFAULT NULL,
+  `initials` varchar(2) DEFAULT NULL,
+  `color` varchar(7) DEFAULT NULL,
   `groups_groupcode` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -57,10 +86,12 @@ CREATE TABLE `positions` (
 -- Dumping data for table `positions`
 --
 
-INSERT INTO `positions` (`id`, `position`, `uniqueID`, `groups_groupcode`) VALUES
-(270, 'LatLng(63.10, 21.61)', 'testtest11', 'lDzWvR1wei'),
-(375, 'LatLng(63.09, 21.65)', 'testtest22', 'lDzWvR1wei'),
-(381, 'LatLng(63.10251, 21.61823)', 'al6cR5DgwD', 'lDzWvR1wei');
+INSERT INTO `positions` (`id`, `position`, `uniqueID`, `initials`, `color`, `groups_groupcode`) VALUES
+(270, 'LatLng(63.10, 21.61)', 'testtest11', 'TE', '#bbbbbb', 'lDzWvR1wei'),
+(375, 'LatLng(63.09, 21.65)', 'testtest22', 'CM', '#979f48', 'lDzWvR1wei'),
+(665, 'LatLng(63.08, 21.67)', 'testtest44', 'JO', '#aaaaaa', 'lDzWvR1wei'),
+(806, 'LatLng(63.10251, 21.61823)', 'dB9O5NtN4K', 'KO', '#AAACCB', 'lDzWvR1wei'),
+(811, 'LatLng(63.10251, 21.61823)', 'klqSA8MK2O', 'MK', '#5BC0EB', 'lDzWvR1wei');
 
 --
 -- Indexes for dumped tables
@@ -70,6 +101,12 @@ INSERT INTO `positions` (`id`, `position`, `uniqueID`, `groups_groupcode`) VALUE
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -86,13 +123,19 @@ ALTER TABLE `positions`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=382;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=812;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
