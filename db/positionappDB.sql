9ccae4be4fae2f8db7fccd3a9c2121a0d8e4dcef
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2022-08-16 12:59:16 +0300

    Update positionappDB.sql

--- db/positionappDB.sql
+++ db/positionappDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 12, 2022 at 12:41 PM
+-- Generation Time: Aug 16, 2022 at 11:58 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `positionappDB`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goals`
+--
+
+CREATE TABLE `goals` (
+  `id` int(4) NOT NULL,
+  `positions` varchar(255) DEFAULT NULL,
+  `groups_groupcode` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `goals`
+--
+
+INSERT INTO `goals` (`id`, `positions`, `groups_groupcode`) VALUES
+(2, 'LatLng(63.10451, 21.62023),LatLng(63.10651, 21.62223),LatLng(63.10851, 21.62423),LatLng(63.11051, 21.62623),LatLng(63.11251, 21.62823)', 'lDzWvR1wei');
 
 -- --------------------------------------------------------
 
@@ -65,7 +84,9 @@ INSERT INTO `messages` (`id`, `message`, `initials`, `color`, `groups_groupcode`
 (31, 'testing', 'TE', '#FF0000', 'lDzWvR1wei'),
 (32, 'this is a longer message for testing the textboxes', 'TE', '#FF0000', 'lDzWvR1wei'),
 (33, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', 'MK', '#5BC0EB', 'lDzWvR1wei'),
-(34, 'hello', 'KO', '#AAACCB', 'lDzWvR1wei');
+(34, 'hello', 'KO', '#AAACCB', 'lDzWvR1wei'),
+(35, 'test', 'LO', '#FF0000', 'lDzWvR1wei'),
+(36, 'test', 'MK', '#5BC0EB', 'lDzWvR1wei');
 
 -- --------------------------------------------------------
 
@@ -87,15 +108,21 @@ CREATE TABLE `positions` (
 --
 
 INSERT INTO `positions` (`id`, `position`, `uniqueID`, `initials`, `color`, `groups_groupcode`) VALUES
-(270, 'LatLng(63.10, 21.61)', 'testtest11', 'TE', '#bbbbbb', 'lDzWvR1wei'),
+(270, 'LatLng(63.075, 21.66)', 'testtest11', 'TE', '#bbbbbb', 'lDzWvR1wei'),
 (375, 'LatLng(63.09, 21.65)', 'testtest22', 'CM', '#979f48', 'lDzWvR1wei'),
 (665, 'LatLng(63.08, 21.67)', 'testtest44', 'JO', '#aaaaaa', 'lDzWvR1wei'),
-(806, 'LatLng(63.10251, 21.61823)', 'dB9O5NtN4K', 'KO', '#AAACCB', 'lDzWvR1wei'),
-(811, 'LatLng(63.10251, 21.61823)', 'klqSA8MK2O', 'MK', '#5BC0EB', 'lDzWvR1wei');
+(1046, 'LatLng(63.10251, 21.61823)', 'llrpH4b8ko', 'MK', '#5BC0EB', 'lDzWvR1wei'),
+(1054, 'LatLng(63.10251, 21.61823)', 'JTfGWKy5GC', 'MK', '#5BC0EB', 'lDzWvR1wei');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `goals`
+--
+ALTER TABLE `goals`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `groups`
@@ -120,22 +147,28 @@ ALTER TABLE `positions`
 --
 
 --
+-- AUTO_INCREMENT for table `goals`
+--
+ALTER TABLE `goals`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=812;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1055;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
