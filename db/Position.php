7939d1b90e478fb56f05d83426480b73e73630ed
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2022-09-16 12:01:44 +0300

    Worked on Position class

--- db/Position.php
+++ db/Position.php
@@ -2,21 +2,57 @@
 
 class Position
 {
+    private const TABLE_NAME = 'positions';
+    private const FIELD_LAT = 'lat';
+    private const FIELD_LNG = 'lng';
 
-    /** @var number */
-    private $longitude;
+    /** @var id */
+    private $id;
 
-    /** @var number */
+    /** @var decimal */
     private $latitude;
 
-    public function __construct($longitude, $latitude)
+    /** @var decimal */
+    private $longitude;
+
+    // Varför är det understryckningar före funktion namnet?
+    public function __construct($latitude, $longitude)
     {
-        $this->longitude = $longitude;
         $this->latitude = $latitude;
+        $this->longitude = $longitude;
     }
 
     public function __toString()
     {
         return 'string eqvivalent...';
+    }
+
+    public function save(): void
+    {
+        if (empty($this->id)) {
+            $this->insert();
+        } else {
+            $this->update();
+        }
+    }
+
+    private function insert(): void
+    {
+        $pdo = dbHandler::getPdbConnection();
+        $stmt = $pdo->prepare('INSERT INTO ' . self::TABLE_NAME . ' (' . self::FIELD_LAT . ', ' . self::FIELD_LNG . ') VALUES (?, ?)');
+        $stmt->bindParam(1, $this->latitude);
+        $stmt->bindParam(2, $this->longitude);
+        $stmt->execute();
+        $this->id = $pdo->lastInsertId();
+    }
+
+    private function update(): void
+    {
+        $pdo = dbHandler::getPdbConnection();
+        $stmt = $pdo->prepare('UPDATE ' . self::TABLE_NAME . ' SET ' . self::FIELD_LAT . ' =  ?, ' . self::FIELD_LNG . ' = ? WHERE ID = ?');
+        $stmt->bindParam(1, $this->startPosition);
+        $stmt->bindParam(2, $this->goalPosition);
+        $stmt->bindParam(3, $this->id);
+        $stmt->execute();
     }
 }
--- db/User.php
+++ db/User.php
@@ -2,5 +2,19 @@
 
 class User
 {
-    private const TABLE_NAME = "users";
+    private const TABLE_NAME = 'users';
+    private const FIELD_LAT = 'lat';
+    private const FIELD_LNG = 'lng';
+    private const FIELD_UNIQUE_ID = 'uniqueID';
+    private const FIELD_INITIALS = 'initials';
+    private const FIELD_COLOR = 'color';
+    private const FIELD_GROUPCODE = 'groups_groupcode';
+
+    /** @var int */
+    private $id;
+
+    /** @var decimal */
+    public $position;
+
+    // TODO: Create a positions table in database so that we can use Position class properly
 }
