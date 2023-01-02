c60ace820a7188146b0103e00bd91226dd1122b5
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-01-02 13:05:51 +0200

    Update positionappDB.sql

--- copy-of-db/positionappDB.sql
+++ copy-of-db/positionappDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 05, 2022 at 05:04 PM
+-- Generation Time: Jan 02, 2023 at 12:51 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -84,7 +84,17 @@ INSERT INTO `messages` (`id`, `message`, `imagepath`, `dateofmessage`, `timeofme
 (181, 'test', NULL, '2022-11-22', '15:44:00', 5646, 'AA', '#FF0000', '01A'),
 (186, 'test', NULL, '2022-11-30', '17:12:00', 6604, 'AA', '#000000', '01A'),
 (191, 'test', NULL, '2022-11-30', '18:39:00', 6602, 'MK', '#5BC0EB', '01A'),
-(192, 'test', NULL, '2022-11-30', '18:40:00', 6602, 'MK', '#5BC0EB', '01A');
+(192, 'test', NULL, '2022-11-30', '18:40:00', 6602, 'MK', '#5BC0EB', '01A'),
+(200, 'test', NULL, '2022-12-05', '17:24:00', 6616, 'AA', '#FF0000', '01A'),
+(201, 'test', NULL, '2022-12-07', '18:26:00', 6617, 'AA', '#FF0000', '01A'),
+(202, 'test', NULL, '2022-12-07', '18:32:00', 6602, 'MK', '#5BC0EB', '01A'),
+(203, 'test', NULL, '2022-12-07', '18:32:00', 6618, 'AA', '#FF0000', '01A'),
+(205, 'test', NULL, '2022-12-07', '18:54:00', 6602, 'MK', '#5BC0EB', '01A'),
+(206, 'test', NULL, '2022-12-07', '18:55:00', 6602, 'MK', '#5BC0EB', '01A'),
+(207, 'test', NULL, '2022-12-07', '19:51:00', 6602, 'MK', '#5BC0EB', '01A'),
+(209, 'test', NULL, '2022-12-13', '15:08:00', 6602, 'MK', 'skyblue', '01A'),
+(210, 'test', NULL, '2022-12-16', '17:33:00', 6602, 'MK', 'skyblue', '01A'),
+(211, 'test', NULL, '2022-12-16', '17:39:00', 6622, 'AA', 'blue', '01A');
 
 -- --------------------------------------------------------
 
@@ -105,13 +115,8 @@ CREATE TABLE `positions` (
 INSERT INTO `positions` (`id`, `lat`, `lng`) VALUES
 (2, '63.115000', '21.635000'),
 (3, '63.117500', '21.634240'),
-(118865, '63.119140', '21.663666'),
-(118866, '63.155130', '21.672592'),
-(134152, '63.104614', '21.620326'),
-(134159, '63.104614', '21.620326'),
-(134172, '63.104614', '21.620326'),
-(134182, '63.104614', '21.620326'),
-(134185, '63.104614', '21.620326');
+(4, '63.124000', '21.634000'),
+(5, '63.088000', '21.623000');
 
 -- --------------------------------------------------------
 
@@ -133,7 +138,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `positions_id`, `initials`, `color`, `groups_groupcode`) VALUES
 (5633, 2, 'FF', '#FFBBAA', '01A'),
-(5634, 3, 'GG', '#00FF00', '01A');
+(5634, 3, 'GG', '#00FF00', '01A'),
+(6619, 4, 'HH', '#ACF000', '01A'),
+(6620, 5, 'JJ', '#49212A', '01A');
 
 -- --------------------------------------------------------
 
@@ -195,37 +202,37 @@ ALTER TABLE `waypoints`
 -- AUTO_INCREMENT for table `goals`
 --
 ALTER TABLE `goals`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2172;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2389;
 
 --
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=200;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=213;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134660;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=162944;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6616;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6797;
 
 --
 -- AUTO_INCREMENT for table `waypoints`
 --
 ALTER TABLE `waypoints`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124447;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146147;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
